52e05d87b4defb418809ebed08ca467b93c1b4b2
Chrustkiran Sathiyananthadevar <christkiran.15@cse.mrt.ac.lk>
2020-08-18 01:59:30 +0530

    added https

--- src/service/MedHistoryService.js
+++ src/service/MedHistoryService.js
@@ -1,6 +1,6 @@
 export const MedHistoryService = {
     getMedHistory : () => {
-        const url = 'http://' + process.env.REACT_APP_BACKEND + '/get-all-med-history';
+        const url = 'https://' + process.env.REACT_APP_BACKEND + '/get-all-med-history';
         return fetch(url)
             .then(response => response.json())
             .catch(err => console.log(err));
